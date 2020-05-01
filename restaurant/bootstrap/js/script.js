9234d2230943c67fc92f6d67cfd020807bfb15d4
Anis-Dhaoui <61118805+Anis-Dhaoui@users.noreply.github.com>
2020-05-01 02:40:03 +0100

    Update script.js

--- restaurant/bootstrap/js/script.js
+++ restaurant/bootstrap/js/script.js
@@ -1,11 +1,1 @@
-$(function (){ 
-//document.addEventListener("DOMContentLoaded", function (event){...})
 
-    $("#navbarToggle").blur(function (event) {
-        //document.querySelector("#navbarToggle").addEventListener("blur", function (event){...})
-        var screenWidth = window.innerWidth;
-        if (screenWidth < 768){
-            $("#collapsable-nav").collapse('hide');
-        }
-    });
-})
